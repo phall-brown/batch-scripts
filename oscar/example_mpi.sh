912542a969f1c397c507c632f2de82a45e515fc8
Paul Hall <paul_hall@brown.edu>
2022-12-09 15:36:06 -0500

    Added generic mpi example

---
+++ oscar/example_mpi.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Example batch script for running mpi jobs on Oscar
+#**********************
+# SLURM JOB INFORMATION
+#**********************
+# Walltime requested for job (24 hrs)
+#SBATCH -t 24:00:00
+
+# Request use of 32 cores and 4GB of memory per core on 4 nodes
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=32
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4G
+
+# Define Oscar partition to use
+#SBATCH -p batch
+
+# Job Name
+#SBATCH -J MyMPIJob 
+
+# SLURM output (*.out) and error (*.err) file names
+# Use '%x' for Job Name,'%A' for array-job ID, '%j' for job ID and '%a' for task ID`
+#SBATCH -e %x-%j.err
+#SBATCH -o %x-%j.out
+
+# Notify user if job fails or ends (uncomment and add your email address to use)
+##SBATCH --mail-user=your_name@brown.edu
+##SBATCH --mail-type=FAIL,END
+
+#********************
+# COMMANDS TO EXECUTE
+#********************
+# load desired modules (change to suit your particular needs)
+module load mpi/openmpi_4.0.7_gcc_10.2_slurm22
+module load gcc/10.2 
+# set environment variables (change to suit your needs)
+EXAMPLE_VARIABLE="Hello!"
+# commands to be executed (change to suit your needs)
+echo $EXAMPLE_VARIABLE
+srun --mpi=mpix echo $EXAMPLE_VARIABLE 
